4dc45e91dfca7e93aa04687ad39fda1a6e2a7f9c
Paopao sa <paopaosa@qq.com>
2016-02-04 18:41:04 +0800

    Initial Commit

---
+++ MyRACStudy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyRACStudy
+//
+//  Created by macpps on 16/2/4.
+//  Copyright © 2016年 PPS. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyRACStudy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyRACStudy
+//
+//  Created by macpps on 16/2/4.
+//  Copyright © 2016年 PPS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
