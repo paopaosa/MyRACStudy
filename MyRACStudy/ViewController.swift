69e92c6819044e985b43b5c864420e050631de53
Paopao sa <paopaosa@qq.com>
2016-02-11 21:25:22 +0800

    try observer

--- MyRACStudy/ViewController.swift
+++ MyRACStudy/ViewController.swift
@@ -35,7 +35,16 @@ class ViewController: NSViewController {
         titleSignal.observeNext{ text in
             log.debug("your window title to \(text!)")
         }
-
+        titleSignal.observeFailed { error in
+            log.debug("Failed: \(error)")
+        }
+        titleSignal.observeCompleted {
+            log.debug("Completed")
+        }
+        titleSignal.observeInterrupted {
+            log.debug("Interrupted")
+        }
+        
         DynamicProperty(object: self, keyPath: "title").signal
             .observeNext{ object in
                 log.debug("what is nsviewcontroller title:\(object)")
@@ -43,8 +52,14 @@ class ViewController: NSViewController {
         
         self.title = "1"
         self.title = "2"
-//        let newSignal:ReactiveCocoa.Signal
+
+        let (waitSignal,observer) = Signal<Int, NoError>.pipe()
+        waitSignal.observeNext { (next) -> () in
+            log.debug("wait signal send next: \(next)")
+        }
         
+        observer.sendNext(1)
+        observer.sendNext(0)
         
         let nameSignalProducer = self.username.rac_textSignal().toSignalProducer()
         nameSignalProducer.start { event in
@@ -64,17 +79,31 @@ class ViewController: NSViewController {
             }
         }
         
-        
-        
         NSNotificationCenter.defaultCenter().rac_addObserverForName("MyNotification", object: nil).subscribeNext { notification in
             log.debug("we got my notification:\(notification.debugDescription!)")
         }
         
         let signalA = self.rac_signalForSelector("viewWillAppear")
-        signalA.subscribeNext { AnyObjectA in
+        signalA.subscribeNext { [unowned self]AnyObjectA in
             log.info("your view will Appear:\(self.view.window!.title)")
+            self.createSignal()
         }
         
+    }
+    
+    func createSignal() -> Signal<String, NoError> {
+        var count = 0
+        return Signal {
+            observer in
+//            NSTimer.schedule(repeatInterval: 1.0) { timer in
+//                sink.sendNext("tick #\(count++)")
+//            }
+            timer(1, onScheduler: QueueScheduler.mainQueueScheduler)
+                .startWithNext{ next in
+                    observer.sendNext("tick #\(count++)")
+            }
+            return nil
+        }
     }
     
     override var representedObject: AnyObject? {
